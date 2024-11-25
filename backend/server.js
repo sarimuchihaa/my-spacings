d2e8474b842e86d443d98108aeee415b721ab278
sarimuchihaa <sarimslayerali786@gmail.com>
2024-11-25 08:00:43 +0500

    ok e'

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import express from "express";
 import dotenv from "dotenv";
 import dbConnection from "./db/dbConnection.js";
 import { app } from "./app.js";
+import path from 'path';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 
 
@@ -13,6 +15,13 @@ app.get("/", (req, res) => {
     res.send("Hello Sarim 😇");
 })
 
+if (process.env.NODE_ENV === "production") 
+{
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 // DB Connection.
