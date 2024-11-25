2b6ff33b2705d5c364183362bd4455db0657688b
sarimuchihaa <sarimslayerali786@gmail.com>
2024-11-25 19:11:11 +0500

    Changes now

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 // ROUTES
