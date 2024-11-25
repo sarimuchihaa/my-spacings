d9969c54fb326568ac159a654aea8b111a6a755e
sarimuchihaa <sarimslayerali786@gmail.com>
2024-11-25 19:21:51 +0500

    changes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
-s
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const uploadsDir = path.join(__dirname, "uploads");
