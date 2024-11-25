4a104909f6568f56f3bd66083ffa59edaaef293b
sarimuchihaa <sarimslayerali786@gmail.com>
2024-11-25 19:19:13 +0500

    multer

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 // IMPORTING
+import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+s
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const uploadsDir = path.join(__dirname, "uploads");
+
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir, { recursive: true }); 
+  console.log("Uploads directory created at:", uploadsDir);
+}
 import express from "express";
 import cors from "cors"; 
 import cookieParser from "cookie-parser";
